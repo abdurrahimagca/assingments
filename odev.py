767a146ee9c4d27dc4c4b7c2e961681f1d07900d
My Name <abbdrhm@gmail.com>
2022-12-14 03:59:52 +0300

    py

--- odev.py
+++ odev.py
@@ -10,6 +10,7 @@
 from enum import Enum
 
 
+
 class Winner(Enum):
     LEFT = 1
     RIGHT = 2
@@ -58,20 +59,17 @@ class Chain:
         #for link in range(len(self.__links)):
          #       print(self.__links[link])
         #print("----------------------")
+        
+       
+
         if(link_index !=0 and link_index!=len(self.__links)):
       
-            #finds weakest links' index for left
-            #print("first if executed")
             temp = 0
             i = 0
             while(i < link_index):
                 if(self.__links[i].strength <= self.__links[temp].strength):
                     temp = i
                 i += 1
-                
-            #print(self.__links[temp])
-            
-            #removes weakeast link and other links
             j = temp
             while(j > -1):
                 self.__links[j] = None
@@ -99,7 +97,7 @@ class Chain:
             while(i < len(self.__links)):
                if(self.__links[i].strength <= self.__links[temp].strength):
                    temp = i
-                   print(temp)
+            
                i +=1
 
           
@@ -118,10 +116,7 @@ class Chain:
                 if(self.__links[i].strength <= self.__links[temp].strength):
                     temp = i
                 i += 1
-                
-            #print(self.__links[temp])
             
-            #removes weakeast link and other links
             j = temp
             while(j > -1):
                 self.__links[j] = None
@@ -131,33 +126,79 @@ class Chain:
         else:
             print("something went wrong")
             
-        """print("----------------------")
-
-        for link2 in range(len(self.__links)):
-              print("updated list")
-              print(self.__links[link2])
-            
-        print("----------------------")"""
-
-
-
-    
-        
 
     def get_winner(self):
         """ 
         Kazanan tarafı bulan method.
         Bu method'dan hangi tipte bir sonuç dömesi gerektiğini
         anlamak için test case'leri inceleyiniz.
+
+
         """
-        return None
+        winner = None
+        lenght = len(self.__links)
+        if(lenght % 2 == 0):
+            mid = round(lenght/2)
+            sumLeft, sumRight = 0, 0
+            i = 0
+            while(i < mid):
+                sumLeft += self.__links[i].points
+                i+=1
+            i = mid
+            while(i < lenght):
+                sumRight += self.__links[i].points
+                i+=1
+            if(sumLeft > sumRight):
+                winner = Winner.LEFT
+            elif(sumRight > sumLeft):
+                winner = Winner.RIGHT
+            elif(sumRight == sumLeft):
+                winner = Winner.TIE
+            else:
+                print("something went wrong")
+                return None
+        elif( lenght % 2 != 0):
+            mid = round(lenght/2)
+        
+            sumLeft, sumRight = 0, 0
+            i = 0
+            while(i <= mid):
+                sumLeft += self.__links[i].points
+                i+=1
+            i = mid
+            while(i < lenght):
+                sumRight += self.__links[i].points
+                i+=1
+            if(sumLeft > sumRight):
+                winner = Winner.LEFT
+            elif(sumRight > sumLeft):
+                winner = Winner.RIGHT
+            elif(sumRight == sumLeft):
+                winner = Winner.TIE
+            else:
+                print("something went wrong")
+                return None
+
+
+
+        print(winner)
+        return winner
 
     def __str__(self):
         """ 
         Zinciri test case'lerdeki gibi string'e 
         dönüştürmeniz gerekmektedir.
         """
-        return " -> "
+        stringTemp = ''
+        i = 0
+        while(i < len(self.__links)):
+            stringTemp += f"<S:{self.__links[i].strength}, P:{self.__links[i].points}>"
+            
+            if(i != (len(self.__links)-1)):
+                stringTemp += ' -> '
+            i+=1
+   
+        return stringTemp
 
     # değiştirilebilir alan bitişi
 
@@ -168,7 +209,7 @@ class Chain:
 test_results = []
 
 # test case 1
-print("case 1 ")
+#print("case 1 ")
 chain = Chain()
 links = [Link(5, 90), Link(7, 80), Link(4, 70),
          Link(4, 80), Link(9, 90), Link(4, 50)]
@@ -177,11 +218,13 @@ test_results.append(str(chain) == "<S:5, P:90> -> <S:7, P:80> -> <S:4, P:70> -> 
 chain.hang(5)
 test_results.append(str(chain) == "<S:9, P:90> -> <S:4, P:50>")
 winner = chain.get_winner()
+if(winner == Winner.LEFT):
+    print("case 1 winner dogru")
 test_results.append(winner == Winner.LEFT)
 
-
+""""""
 # test case 2
-print("case 2")
+#print("case 2")
 chain = Chain()
 links = [Link(5, 90), Link(7, 80), Link(4, 70),
          Link(5, 80), Link(9, 90), Link(4, 50)]
@@ -190,10 +233,12 @@ test_results.append(str(chain) == "<S:5, P:90> -> <S:7, P:80> -> <S:4, P:70> -> 
 chain.hang(2)
 test_results.append(str(chain) == "<S:7, P:80> -> <S:4, P:70> -> <S:5, P:80> -> <S:9, P:90>")
 winner = chain.get_winner()
+if(winner == Winner.RIGHT):
+    print("case 2 winner dogru")
 test_results.append(winner == Winner.RIGHT)
 
 # test case 3
-print("case 3")
+#print("case 3")
 chain = Chain()
 links = [Link(3, 90), Link(3, 80), Link(4, 90), Link(5, 80),
          Link(4, 70), Link(4, 80), Link(5, 90), Link(3, 50)]
@@ -202,10 +247,12 @@ test_results.append(str(chain) == "<S:3, P:90> -> <S:3, P:80> -> <S:4, P:90> -> 
 chain.hang(4)
 test_results.append(str(chain) == "<S:4, P:90> -> <S:5, P:80> -> <S:4, P:70> -> <S:4, P:80> -> <S:5, P:90>")
 winner = chain.get_winner()
+if(winner == Winner.TIE):
+    print("case 3 winner dogru")
 test_results.append(winner == Winner.TIE)
 
 # test case 4
-print("case 4")
+#print("case 4")
 chain = Chain()
 links = [Link(3, 90), Link(5, 80), Link(4, 90), Link(5, 80),
          Link(4, 70), Link(4, 80), Link(3, 90), Link(9, 50)]
@@ -216,6 +263,8 @@ chain.hang(0)
 test_results.append(str(
     chain) == "<S:3, P:90> -> <S:5, P:80> -> <S:4, P:90> -> <S:5, P:80> -> <S:4, P:70> -> <S:4, P:80>")
 winner = chain.get_winner()
+if(winner == Winner.RIGHT):
+    print("case 4 winner dogru")
 test_results.append(winner == Winner.RIGHT)
 
 # score
